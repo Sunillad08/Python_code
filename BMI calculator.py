c56bb87f2c444dd210612f69d265199c5d898773
Sunil Lad <ladsunil08@gmail.com>
2021-04-11 21:05:35 +0530

    tried formatting for python

--- BMI calculator.py
+++ BMI calculator.py
@@ -1,36 +1,38 @@
 from tkinter import *
 from tkinter import messagebox
 
+
 def getbmi():
-	h = height.get()
-	w = weight.get()
-	try:
-		h = float(h)
-		w=float(w)
-		h = 0.02540000002590 * h
-	except:
-		bmi.configure(text = "Invalid")
-	else :
-		b = w / (h**2)
-		b = round (b , 2)
-		bmi.configure(text = b)
-		if b < 18.5 :
-			n = "underweight"
-		elif b < 24.9:
-			n = "normal"
-		elif b < 29.9 :
-			n = "overweight"
-		else :
-			n = "obese"
-		wei.configure(text = n)
-	
+    h = height.get()
+    w = weight.get()
+    try:
+        h = float(h)
+        w = float(w)
+        h = 0.02540000002590 * h
+    except:
+        bmi.configure(text="Invalid")
+    else:
+        b = w / (h**2)
+        b = round(b, 2)
+        bmi.configure(text=b)
+        if b < 18.5:
+            n = "underweight"
+        elif b < 24.9:
+            n = "normal"
+        elif b < 29.9:
+            n = "overweight"
+        else:
+            n = "obese"
+        wei.configure(text=n)
+
+
 wn = Tk()
 
 height = Entry(wn)
 weight = Entry(wn)
 bmi = Label(wn)
 wei = Label(wn)
-save = Button(wn ,text= "BMI" ,command = getbmi)
+save = Button(wn, text="BMI", command=getbmi)
 
 weight.pack()
 height.pack()
--- ageverified.py
+++ ageverified.py
@@ -1,11 +1,11 @@
 try:
-	age=int(input("Enter your age = "))
-	if age>17:
-		 vote=input("choose from these:  \n 1.sunil\n2.gaurang\n3.Nota\n")
-	else:
-		print("Underage")
-        
+    age = int(input("Enter your age = "))
+    if age > 17:
+        vote = input("choose from these:  \n 1.sunil\n2.gaurang\n3.Nota\n")
+    else:
+        print("Underage")
+
 except:
-	print("invalid input")
+    print("invalid input")
 finally:
     print("thank!")
