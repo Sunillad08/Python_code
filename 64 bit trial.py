97493c7be3d266843b57f2c53194f17ea697719a
Sunil Lad <ladsunil08@gmail.com>
2021-04-16 10:49:41 +0530

    updated morse configuration

--- 64 bit trial.py
+++ 64 bit trial.py
@@ -1,3 +1,10 @@
 import matplotlib as plt
 
+import time
+start_time = time.time()
+
 print("Hllo")
+time.sleep(0.001)
+
+end_time = time.time()
+print(f"execution time : {end_time - start_time:.8f} sec")
