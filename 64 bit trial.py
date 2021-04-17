8989949c10612e74fd3a93d0ea2be2c3ddd5e768
Sunil Lad <ladsunil08@gmail.com>
2021-04-17 19:36:15 +0530

    did something

--- 64 bit trial.py
+++ 64 bit trial.py
@@ -1,10 +1,4 @@
 import matplotlib as plt
+import subprocess as sp 
 
-import time
-start_time = time.time()
-
-print("Hllo")
-time.sleep(0.001)
-
-end_time = time.time()
-print(f"execution time : {end_time - start_time:.8f} sec")
+sp.run("dir" , shell= True)
